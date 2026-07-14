555bd85003080c670c8c440ccde93bf0cbeaee05
Humberto-lopez28 <jhlopezada@unadvirtual.edu.co>
2026-07-14 15:21:51 -0500

    Update conexion.php

--- config/conexion.php
+++ config/conexion.php
@@ -1,1 +1,20 @@
+<?php
+// config/conexion.php
+class Conexion {
+    public static function conectar() {
+        $host = "localhost";
+        $db = "agropuerto";
+        $user = "root";
+        $password = ""; // En Codespaces por defecto va vacío
+        
+        try {
+            $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $password);
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $pdo;
+        } catch (Exception $e) {
+            die("Error de conexión: " . $e->getMessage());
+        }
+    }
+}
+?>
 
